84f0a3241b8f4f3dda022f61b829cf86d0ddb610
Zhentian Zhang <zz51@illinois.edu>
2022-02-04 23:53:17 -0600

    try string

--- vector/sstring.c
+++ vector/sstring.c
@@ -14,39 +14,90 @@
 
 struct sstring {
     // Anything you want
+    char *str;
+    size_t size;
 };
 
 sstring *cstr_to_sstring(const char *input) {
     // your code goes here
-    return NULL;
+    sstring* str = malloc(sizeof(sstring));
+    str->str = strdup(input);
+    str->size = strlen(input);
+    return str;
 }
 
 char *sstring_to_cstr(sstring *input) {
     // your code goes here
-    return NULL;
+    char *ret = strdup(input->str);
+    return ret;
 }
 
 int sstring_append(sstring *this, sstring *addition) {
     // your code goes here
-    return -1;
+    this->str = realloc(this->str, this->size + addition->size + 1);
+    strcpy(this->str + this->size, addition->str);
+    this->size += addition->size;
+    return this->size;
 }
 
 vector *sstring_split(sstring *this, char delimiter) {
     // your code goes here
-    return NULL;
+    vector* vec = string_vector_create();
+    char* t = strdup(this->str);
+    char* tmp = t;
+    char* startOfWord = tmp;
+    while (*tmp) {
+        if (*tmp == delimiter) {
+            *tmp = '\0';
+            char* tmppp = strdup(startOfWord);
+            vector_push_back(vec, tmppp);
+            free(tmppp);
+            if (*(tmp+1)) {
+                startOfWord = tmp + 1;
+            } else {
+                vector_push_back(vec, "");
+                free(t);
+                return vec;
+            }
+        }
+        tmp++;
+    }
+    char* tmpppp = strdup(startOfWord);
+    vector_push_back(vec, tmpppp);
+    free(tmpppp);
+    free(t);
+    return vec;
 }
 
 int sstring_substitute(sstring *this, size_t offset, char *target,
                        char *substitution) {
     // your code goes here
-    return -1;
+    if (offset > this->size) return -1;
+    char * startTargetARest = strstr(this->str+offset, target);
+    if (!startTargetARest) return -1;
+    char * newStr = malloc(this->size - strlen(target) + strlen(substitution) + 1);
+    strncpy(newStr, this->str, this->size - strlen(startTargetARest));
+    newStr[this->size - strlen(startTargetARest)] = '\0';
+    char * endTargetARest = startTargetARest+ strlen(target);
+    strcat(newStr, substitution);
+    strcat(newStr, endTargetARest);
+    free(this->str);
+    this->str = newStr;
+    this->size = this->size - strlen(target) + strlen(substitution);
+
+    return 0;
 }
 
 char *sstring_slice(sstring *this, int start, int end) {
     // your code goes here
-    return NULL;
+    char *ret = malloc(end - start + 1);
+    strncpy(ret, this->str + start, end-start);
+    ret[end-start] = '\0';
+    return ret;
 }
 
 void sstring_destroy(sstring *this) {
     // your code goes here
+    free(this->str);
+    free(this);
 }
--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -3,8 +3,58 @@
  * CS 241 - Spring 2022
  */
 #include "sstring.h"
-
+#include <assert.h>
+#include <string.h>
 int main(int argc, char *argv[]) {
     // TODO create some tests
+    sstring *str1 = cstr_to_sstring("abc");
+    char *str1_c = sstring_to_cstr(str1);
+    assert(!strcmp(str1_c, "abc"));
+    free(str1_c);
+    sstring *str2 = cstr_to_sstring("def");
+    int size = sstring_append(str1, str2);
+    assert(size == 6);
+    char *tmp = sstring_to_cstr(str1);
+    assert(!strcmp(tmp, "abcdef"));
+    free(tmp);
+
+    vector *v1 = sstring_split(str1, 'c');
+    assert(vector_size(v1) == 2);
+    assert(!strcmp((char*)vector_get(v1, 0), "ab"));
+    assert(!strcmp((char*)vector_get(v1, 1), "def"));
+
+    vector *v2 = sstring_split(str1, 'a');
+    assert(vector_size(v2) == 2);
+    assert(!strcmp((char*)vector_get(v2, 0), ""));
+    assert(!strcmp((char*)vector_get(v2, 1), "bcdef"));
+
+    vector *v3 = sstring_split(str1, 'f');
+    assert(vector_size(v3) == 2);
+    assert(!strcmp((char*)vector_get(v3, 0), "abcde"));
+    assert(!strcmp((char*)vector_get(v3, 1), ""));
+
+    sstring *str3 = cstr_to_sstring("This is a {} day, {}!");
+    sstring_substitute(str3, 18, "{}", "friend");
+    char *tmpp = sstring_to_cstr(str3);
+    assert(!strcmp("This is a {} day, friend!", tmpp));
+    free(tmpp);
+
+    sstring *str4 = cstr_to_sstring("1234567890");
+    char* tmppp = sstring_slice(str4, 2, 5);
+    assert(!strcmp(tmppp, "345"));
+    free(tmppp);
+
+    sstring_destroy(str1);
+    sstring_destroy(str2);
+    sstring_destroy(str3);
+    sstring_destroy(str4);
+
+    vector_destroy(v1);
+    vector_destroy(v2);
+    vector_destroy(v3);
+
+
+    // int len = sstring_append(str1, str2); // len == 6
+    // sstring_to_cstr(str1); // == "abcdef"
     return 0;
 }
