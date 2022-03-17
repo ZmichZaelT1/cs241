763282887ecb07f04359b34756bd98a720fcc936
Zhentian Zhang <zz51@illinois.edu>
2022-03-16 20:36:16 -0500

    mp

--- password_cracker/cracker1.c
+++ password_cracker/cracker1.c
@@ -5,10 +5,78 @@
 #include "cracker1.h"
 #include "format.h"
 #include "utils.h"
+#include "./includes/queue.h"
+#include <string.h>
+#include <stdio.h>
+#include <crypt.h>
+#include <math.h>
+
+static queue *task_q = NULL;
+static int queue_size = 0;
+static int success = 0;
+static int failed = 0; 
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER; 
+
+void *cracker(void *i) {
+    char *curr = NULL;
+    char user[20], hash[20], known[20];
+    while ((curr = queue_pull(task_q)) != NULL) {
+        sscanf(curr, "%s %s %s", user, hash, known);
+        v1_print_thread_start((long) i, user);
+        double start = getThreadCPUTime();
+        int prefixLength = getPrefixLength(known);
+        int unknownLength = strlen(known) - prefixLength;
+
+        struct crypt_data cdata;
+        cdata.initialized = 0;
+        char *unknown = known + prefixLength;
+        setStringPosition(unknown, 0);
+        int hashCount = 0;
+        int result = 1;
+        for (int i = 0; i < pow(26, unknownLength); i++) {
+            char *guess = crypt_r(known, "xx", &cdata);
+            hashCount++;
+            if (!strcmp (guess, hash)) {
+                pthread_mutex_lock(&mut);
+                success++;
+                pthread_mutex_unlock(&mut);
+                result = 0;
+                break;
+            }
+            incrementString(known);
+        }
+        v1_print_thread_result((long) i, user, known, hashCount, getThreadCPUTime() - start, result);
+    }
+    queue_push(task_q, NULL);
+    return NULL;
+}
 
 int start(size_t thread_count) {
     // TODO your code here, make sure to use thread_count!
     // Remember to ONLY crack passwords in other threads
+
+    task_q = queue_create(-1);
+    char *buf = NULL;
+    size_t buf_size = 0;
+
+    while (getline(&buf, &buf_size, stdin) != -1) {
+        queue_push(task_q, strdup(buf));
+        queue_size++;
+    }
+    queue_push(task_q, NULL);
+
+    pthread_t pid[thread_count];
+    for (size_t i = 0; i < thread_count; i++) {
+        pthread_create(&pid[i], NULL, cracker, (void*) i+1);
+    }
+    for (size_t i = 0; i < thread_count; i++) {
+        pthread_join(pid[i], NULL);
+    }
+
+    v1_print_summary(success, failed);
+    pthread_mutex_destroy(&mut);
+    queue_destroy(task_q);
+
     return 0; // DO NOT change the return code since AG uses it to check if your
               // program exited normally
 }
--- password_cracker/cracker2.c
+++ password_cracker/cracker2.c
@@ -5,10 +5,137 @@
 #include "cracker2.h"
 #include "format.h"
 #include "utils.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <crypt.h>
+
+// typedef struct task_ {
+//     pthread_t pid;
+//     int worker_id;
+//     int count;
+// } task;
+
+// typedef struct task_ {
+//     char user[20];
+//     char hash[20];
+//     char known[20];
+// } task_struct;
+
+
+static int success = 0;
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t start_b;
+pthread_barrier_t end_b;
+
+static int total_hashCount = 0;
+// static char *result = NULL;
+static int eof = 0;
+static int thread_number = 0;
+static char user[20];
+static char hash[20];
+static char known[20];
+static char result[20];
+
+
+void *cracker(void *i) {
+    struct crypt_data cdata;
+    cdata.initialized = 0;
+    while(1){
+        pthread_barrier_wait(&start_b);
+        if(eof) break;
+        int hashCount = 0;
+        long start_index = 0;
+        long count = 0;
+        int prefixLength = getPrefixLength(known);
+        int unknown_letter_count = strlen(known) - prefixLength;
+        getSubrange(unknown_letter_count, thread_number, (long) i, &start_index, &count);
+
+        char *known_cpy = calloc(strlen(known) + 1 + prefixLength, sizeof(char));
+        known_cpy = strdup(known);
+        char *unknown = known_cpy + prefixLength;
+        setStringPosition(unknown, start_index);
+        v2_print_thread_start((long) i, user, start_index, known_cpy);
+
+        for (int j = 0; j < count; j++){
+            char *guess = crypt_r(known_cpy, "xx", &cdata);
+            hashCount++;
+
+            if(!strcmp(guess, hash)){
+                pthread_mutex_lock(&mut);
+                // result = calloc(1, strlen(known_cpy) + 1);
+                strcpy(result, known_cpy);
+                // result = known_cpy;
+                success = 1;
+                v2_print_thread_result((long) i, hashCount, 0);
+                total_hashCount = total_hashCount + hashCount;
+                pthread_mutex_unlock(&mut);
+                break;
+            }
+
+            if(success){
+                pthread_mutex_lock(&mut);
+                v2_print_thread_result((long) i, hashCount, 1);
+                total_hashCount = total_hashCount + hashCount;
+                pthread_mutex_unlock(&mut);
+                break;
+            }
+
+            incrementString(known_cpy);
+        }
+        free(known_cpy);
+
+        if(!success){
+            pthread_mutex_lock(&mut);
+            v2_print_thread_result((long) i, hashCount, 2);
+            total_hashCount = total_hashCount + hashCount;
+            pthread_mutex_unlock(&mut);
+        }
+        pthread_barrier_wait(&end_b);
+    }
+    return NULL;
+}
 
 int start(size_t thread_count) {
     // TODO your code here, make sure to use thread_count!
     // Remember to ONLY crack passwords in other threads
+    thread_number = thread_count;
+
+    pthread_barrier_init(&start_b, NULL, thread_count + 1);
+    pthread_barrier_init(&end_b, NULL, thread_count + 1);
+
+    pthread_t pid[thread_count];
+    for (size_t i = 0; i < thread_count; i++) {
+        pthread_create(&pid[i], NULL, cracker, (void*) i+1);
+    }
+
+    char *buf = NULL;
+    size_t buf_size = 0;
+    while (getline(&buf, &buf_size, stdin) != -1) {
+        sscanf(buf, "%s %s %s", user, hash, known);
+        v2_print_start_user(user);
+
+        pthread_barrier_wait(&start_b); // start cracker
+        double time_start = getTime();
+        double cpu_start = getCPUTime();
+
+        pthread_barrier_wait(&end_b);
+        v2_print_summary(user, result, total_hashCount, getTime() - time_start, getCPUTime() - cpu_start, 0);
+
+        success = 0;
+        total_hashCount = 0;
+    }
+    eof = 1;
+    pthread_barrier_wait(&start_b);
+
+    for (size_t i = 0; i < thread_count; i++) {
+        pthread_join(pid[i], NULL);
+    }
+    pthread_mutex_destroy(&mut);
+    pthread_barrier_destroy(&start_b);
+    pthread_barrier_destroy(&end_b);
+
     return 0; // DO NOT change the return code since AG uses it to check if your
               // program exited normally
 }
