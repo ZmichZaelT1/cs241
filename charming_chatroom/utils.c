cc39c753ac9b0f5320d3baf7bc695fd4d440f7b9
Zhentian Zhang <zz51@illinois.edu>
2022-04-13 22:18:29 -0500

    fixed

--- charming_chatroom/utils.c
+++ charming_chatroom/utils.c
@@ -45,11 +45,14 @@ ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
         ssize_t bytes_read = read(socket, buffer + bytes_written, count - bytes_written);
         if (bytes_read == 0) {
             return 0;
-        } else if (bytes_read == -1 && errno != EINTR) {
-            break;
-        } else {
-            bytes_written += bytes_read;
         }
+        if (bytes_read == -1 && errno == EINTR) {
+            continue;
+        }
+        if (bytes_read == -1) {
+            return -1;
+        }
+        bytes_written += bytes_read;
     }
     return count;
 }
@@ -61,11 +64,14 @@ ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
         ssize_t bytes_written = write(socket, buffer + bytes_read, count - bytes_read);
         if (bytes_written == 0) {
             return 0;
-        } else if (bytes_written == -1 && errno != EINTR) {
-            break;
-        } else {
-            bytes_read += bytes_written;
+        } 
+        if (bytes_written == -1 && errno == EINTR) {
+            continue;
+        } 
+        if (bytes_written == -1) {
+            return -1;
         }
+        bytes_read += bytes_written;
     }
     return count;
 }
