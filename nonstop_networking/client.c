d5a6269cba97dbe93768a1a7049a11994c365225
Zhentian Zhang <zz51@illinois.edu>
2022-04-14 23:33:22 -0500

    partially done

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -9,13 +9,93 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
 
 #include "common.h"
 
 char **parse_args(int argc, char **argv);
 verb check_args(char **args);
+void *get_in_addr(struct sockaddr*);
+void run_verb(char*, char*);
+void read_LIST();
+void read_GET();
+void read_error(char*);
+void closeConnection();
+
+static volatile int sock_fd = 0;
+static int packet_size = 1000;
+
 int main(int argc, char **argv) {
     // Good luck!
+    if (argc < 3) {
+        print_client_usage();
+        return 0;
+    } 
+    char ** args = parse_args(argc, argv);
+    verb v = check_args(args);
+    
+    char *host = args[0];
+    char *port = args[1];
+    char *v_c = args[2];
+    char *remote = args[3];
+    char *local = args[4];
+
+
+    //////////////////////establish connection///////////////////////////
+    struct addrinfo hints, *servinfo;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    int rv = getaddrinfo(host, port, &hints, &servinfo);
+    if (rv != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    }
+
+    sock_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (sock_fd == -1) {
+        perror("client: socket");
+        exit(1);
+    }
+
+    int ok = connect(sock_fd, servinfo->ai_addr, servinfo->ai_addrlen);
+    if (ok == -1) {
+        perror("Client: connect");
+        exit(1);
+    }
+
+    // char str[INET6_ADDRSTRLEN];
+	// inet_ntop(servinfo->ai_family, get_in_addr((struct sockaddr *)servinfo->ai_addr),
+	// 		str, sizeof str);
+	// fprintf(stderr, "client: connecting to %s\n", str);
+    LOG("connecting");
+    freeaddrinfo(servinfo); 
+    //////////////////////establish connection///////////////////////////
+
+
+
+    if (v == GET) {
+        run_verb(v_c, remote);
+        LOG("GET!");
+        read_GET(local);
+    } else if (v == PUT) {
+        // run_PUT();
+        LOG("PUT!");
+    } else if (v == DELETE) {
+        // run_DELETE();
+        LOG("DELETE!");
+    } else 
+    if (v == LIST) {
+        run_verb(v_c, remote);
+        LOG("LIST!");
+        read_LIST();
+    }
+
+    closeConnection();
+    free(args);
 }
 
 /**
@@ -105,3 +185,160 @@ verb check_args(char **args) {
     print_client_help();
     exit(1);
 }
+
+void *get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void read_LIST() {
+    char *ok = "OK\n";
+    char *buf = calloc(1, strlen(ok) + 1);
+    ssize_t bytes_read = read_all_from_socket(sock_fd, buf, strlen(ok));
+    if (bytes_read != (ssize_t)strlen(ok)) {
+        print_connection_closed();
+        exit(1);
+    }
+
+    if (!strcmp(buf, ok)) { // if OK
+        printf("%s", ok);
+        size_t message_size;
+        ssize_t bytes_read = read_all_from_socket(sock_fd, (char*)&message_size, sizeof(message_size));
+        if (bytes_read != (ssize_t) sizeof(message_size)) {
+            print_connection_closed();
+            exit(1);
+        }
+        
+        char *list_info = calloc(1, sizeof(char) * (message_size));
+        ssize_t info_bytes = read_all_from_socket(sock_fd, list_info, message_size);
+        if (info_bytes == 0 || info_bytes == -1) {
+            print_connection_closed();
+            exit(1);
+        } else if (info_bytes < (ssize_t)message_size) {
+            print_too_little_data();
+            exit(1);
+        } else if (info_bytes > (ssize_t) message_size) {
+            print_received_too_much_data();
+            exit(1);
+        }
+        printf("%zu%s", message_size, list_info);
+
+    } else { // if ERROR
+        read_error(buf);
+    }
+    free(buf);
+}
+
+void run_verb(char *v, char *file) {
+    if (!strcmp(v, "LIST")) {
+        char *request = "LIST\n";
+        ssize_t bytes_written = write_all_to_socket(sock_fd, request, strlen(request));
+        if (bytes_written != (ssize_t)strlen(request)) {
+            print_connection_closed();
+            exit(1);
+        }
+    } else {
+        char request[3 + strlen(v) + strlen(file)];
+        sprintf(request, "%s %s\n", v, file);
+        ssize_t bytes_written = write_all_to_socket(sock_fd, request, strlen(request));
+        if (bytes_written != (ssize_t)strlen(request)) {
+            print_connection_closed();
+            exit(1);
+        }
+    }
+}
+
+void read_GET(char *local) {
+    char *ok = "OK\n";
+    char *buf = calloc(1, strlen(ok) + 1);
+    ssize_t bytes_read = read_all_from_socket(sock_fd, buf, strlen(ok));
+    if (bytes_read != (ssize_t)strlen(ok)) {
+        print_connection_closed();
+        exit(1);
+    }
+
+    if (!strcmp(buf, ok)) { // if OK
+        printf("%s", ok);
+        size_t message_size;
+        ssize_t bytes_read = read_all_from_socket(sock_fd, (char*)&message_size, sizeof(message_size));
+        if (bytes_read != (ssize_t) sizeof(message_size)) {
+            print_connection_closed();
+            exit(1);
+        }
+        printf("%zu", message_size);
+        
+        FILE *fd;
+        fd = fopen(local, "w+");
+        if (!fd) {
+            perror("open file");
+            exit(1);
+        }
+
+        bytes_read = 0;
+        size_t should_read;
+        while (bytes_read < (ssize_t)message_size) {
+            if (message_size - bytes_read > (size_t)packet_size) {
+                should_read = packet_size;
+            } else {
+                should_read = message_size - bytes_read;
+            }
+            char buffer[should_read+1];
+            size_t bytes_received = read_all_from_socket(sock_fd, buffer, should_read);
+            if (bytes_received == 0) break;
+            if (bytes_received != should_read) {
+                print_connection_closed();
+                exit(1);
+            }
+            fwrite(buffer, should_read, 1, fd);
+            bytes_read += should_read;
+        } 
+        if (bytes_read < (ssize_t)message_size) {
+            print_too_little_data();
+            exit(1);
+        } else if (bytes_read > (ssize_t) message_size) {
+            print_received_too_much_data();
+            exit(1);
+        }
+    } else { // if ERROR
+        read_error(buf);
+    }
+    free(buf);
+}
+
+void read_error(char *buf) {
+    char *ok = "OK\n";
+    char *error = "ERROR\n";
+    buf = realloc(buf, strlen(error) + 1);
+    ssize_t bytes_read = read_all_from_socket(sock_fd, buf + strlen(ok), strlen(error) - strlen(ok));
+    if (bytes_read != (ssize_t)(strlen(error) - strlen(ok))) {
+        print_connection_closed();
+        exit(1);
+    }
+
+
+    if (strcmp(buf, error)) {
+        print_invalid_response();
+    } else {
+        char *errorMessage = calloc(1, sizeof(char) * 100);
+        ssize_t bytes_read = read_all_from_socket(sock_fd, errorMessage, sizeof(char) * 100);
+        if (bytes_read != (ssize_t)strlen(errorMessage)) {
+            print_connection_closed();
+            exit(1);
+        }
+        print_error_message(errorMessage);
+        free(errorMessage);
+    }
+}
+
+void closeConnection() {
+    if (shutdown(sock_fd, SHUT_RDWR) != 0) {
+        perror("shutdown");
+    }
+    if (close(sock_fd) != 0) {
+        perror("close");
+    }
+}
+
--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -3,3 +3,53 @@
  * CS 241 - Spring 2022
  */
 #include "common.h"
+
+
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+
+
+ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
+    // Your Code Here
+    size_t bytes_written = 0;
+    while (bytes_written < count) {
+        ssize_t bytes_read = read(socket, buffer + bytes_written, count - bytes_written);
+        if (bytes_read == 0) {
+            break;
+        }
+        if (bytes_read == -1 && errno == EINTR) {
+            continue;
+        }
+        if (bytes_read == -1) {
+            return -1;
+        }
+        bytes_written += bytes_read;
+    }
+    return bytes_written;
+}
+
+ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
+    // Your Code Here
+    size_t bytes_read = 0;
+    while (bytes_read < count) {
+        ssize_t bytes_written = write(socket, buffer + bytes_read, count - bytes_read);
+        if (bytes_written == 0) {
+            break;
+        } 
+        if (bytes_written == -1 && errno == EINTR) {
+            continue;
+        } 
+        if (bytes_written == -1) {
+            return -1;
+        }
+        bytes_read += bytes_written;
+    }
+    return bytes_read;
+}
